72ba46396e428b0bbddcb5abf6d41e3932c65d52
Francisco De La Cruz <helloquico@gmail.com>
2015-11-14 19:41:26 -0500

    Understand State

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,17 @@ var h = require('./helpers');
 */
 
 var App = React.createClass({
+	getInitialState: function () {
+		return {
+			fishes: {},
+			order: {}
+		};
+	},
+	addFish: function (fish) {
+		var timestamp = (new Date()).getTime();
+		this.state.fishes['fish-' + timestamp] = fish;
+		this.setState({ fishes: this.state.fishes });
+	},
 	render: function () {
 		return (
 			<div className="catch-of-the-day">
@@ -21,11 +32,51 @@ var App = React.createClass({
 					<Header tagline="Fresh Seafood Market" />
 				</div>
 				<Order />
-				<Inventory />
+				<Inventory addFish={this.addFish} />
 			</div>
 		)
 	}
 });
+
+/*
+	 Add Fish Form
+	 <AddFishForm />
+*/
+
+var AddFishForm = React.createClass({
+	createFish: function (event) {
+		// 1. Stop the form from submitting
+		event.preventDefault();
+		// 2. Take the data from the form and create an object
+		var fish = {
+			name: this.refs.name.value, 
+			price: this.refs.price.value,
+			status: this.refs.status.value,
+			desc: this.refs.desc.value,
+			image: this.refs.image.value
+		}
+		
+		// 3. Add the fish to the App State
+		this.props.addFish(fish);
+		this.refs.fishForm.reset();
+	},
+	render: function () {
+		return (
+			<form className="fish-edit" ref="fishForm" onSubmit={this.createFish}>
+				<input type="text" ref="name" placeholder="Fish name" />
+				<input type="text" ref="price" placeholder="Fish price" />
+				<select ref="status">
+					<option value="available">Fresh</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea type="text" ref="desc" placeholder="Desc"></textarea>
+				<input type="text" ref="image" placeholder="URL to Image" />
+				<button type="submit">+ Add item </button>
+			</form>
+		)
+	}
+})
+
 
 /*
 	 Header
@@ -69,7 +120,11 @@ var Order = React.createClass({
 var Inventory = React.createClass({
 	render: function () {
 		return (
-			<p>Inventory</p>
+			<div>
+				<h2>Inventory</h2>
+
+				<AddFishForm {...this.props} />
+			</div>
 		)
 	}
 });
